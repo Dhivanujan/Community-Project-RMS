ddea1efcc0a7282ec54ee3f245a42457af4bbf8f
viyash-24 <viyashviyashan2417@gmail.com>
2026-04-23 08:31:46 +0530

    hook function started

--- components/admin/results/FilterPanel.jsx
+++ components/admin/results/FilterPanel.jsx
@@ -9,4 +9,31 @@ export default function FilterPanel({
     onFetchStudents,
     isLoadingStudents,
     disabled,
-}) 
+}) {
+    const { academicYears = [], departments = [], batches = [], semesters = [], subjects = {} } = config;
+
+    // Get subjects filtered by selected department + semester
+    const departmentSubjects = subjects[filters.department] || [];
+    const filteredSubjects = filters.semester
+        ? departmentSubjects.filter((s) => s.semester === filters.semester)
+        : departmentSubjects;
+
+    const selectedSubject = filteredSubjects.find((s) => s.code === filters.subjectCode);
+
+    const allFiltersSelected =
+        filters.academicYear &&
+        filters.department &&
+        filters.batch &&
+        filters.semester &&
+        filters.subjectCode;
+
+    const canFetchStudents = filters.department && filters.batch;
+
+    const handleSubjectChange = (code) => {
+        const subject = filteredSubjects.find((s) => s.code === code);
+        onFilterChange('subjectCode', code);
+        if (subject) {
+            onFilterChange('subjectName', subject.name);
+            onFilterChange('credits', subject.credits);
+        }
+    };

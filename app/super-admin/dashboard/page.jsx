d6b9a29b8a70dd1b7f3a684dcbbb37a9f408772d
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-05-14 17:18:38 +0530

    add    app/super-admin/ file

---
+++ app/super-admin/dashboard/page.jsx
@@ -0,0 +1,14 @@
+import AdminDashboard from "@/app/admin/page";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/lib/authOptions";
+import { redirect } from "next/navigation";
+
+export default async function SuperAdminDashboardPage() {
+  const session = await getServerSession(authOptions);
+  
+  if (!session || session.user.role !== "SUPER_ADMIN") {
+    redirect("/login");
+  }
+
+  return <AdminDashboard />;
+}
